9261cd731a3b00ec7f95a171e9421bb7cf1f2fd7
justin <kinhanjustin@gmail.com>
2026-04-06 16:05:31 +0200

    deployment

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,20 @@
 import type { NextConfig } from "next";
 
+const isProduction = process.env.NODE_ENV === "production";
+const repoName = 'Train-timetable'
+
 const nextConfig: NextConfig = {
   /* config options here */
+  output: 'export',
+
+  basePath: isProduction ? `/${repoName}` : '',
+  assetPrefix: isProduction ? `/${repoName}` : '',
+
   reactCompiler: true,
+
+  images: {
+    unoptimized: true,
+  },
 };
 
 export default nextConfig;
